7755c77386a6aaca2f8e3280d818b2642a3eaeba
Sara <sameads9@gmail.com>
2022-10-04 11:02:11 -0500

    resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,7 +29,12 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(input: savedBook!): User
+        saveBook(bookId: String,
+            authors: [String],
+            description: String,
+            title: String,
+            image: String,
+            link: String): User
         removeBook(bookId: ID!): User
     }
 `;
