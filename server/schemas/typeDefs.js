8e1be8b471fdaae5172f8a77f3a432d752d74944
Sara <sameads9@gmail.com>
2022-10-04 10:13:42 -0500

    typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+// import the gwl tagged template function
+const { gql } = require('apollo-server-express');
+
+// create typeDefs
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        _id: ID
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: savedBook!): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
